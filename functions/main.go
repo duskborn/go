5323556b62f6b50440f1fd5a2359d079e5f48ff8
Ildar Sirazutdinov <mozhgru@gmail.com>
2016-07-26 23:18:49 +0400

    File main.go modified.

--- functions/main.go
+++ functions/main.go
@@ -2,15 +2,24 @@ package main
 
 import "fmt"
 
-func average(xs []float64) float64 {
-    total := 0.0
+func sum(xs ...int) int{    
+    total := 0
     for _, v := range xs {
         total += v
     }
-    return total / float64(len(xs))
+    return total
+}
+	
+func evenCheck(in int) bool{
+if (in%2==0) {
+return true
+}else {
+return false
+}
 }
 
 func main() {
-    xs := []float64{98,93,77,82,83}
-	fmt.Println(average(xs))
+xs := []int{1,2,3}
+var in int = sum(xs ...)
+fmt.Println(evenCheck(in))
 }
